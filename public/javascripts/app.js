2b7d9832a598763d1633c048f6f0f00918acb46b
Corey Yaspan <cspan00@gmail.com>
2016-03-04 19:11:50 -0700

    include production and dev id's for facebook

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,11 @@
 var app = angular.module('topsoil', ['ngRoute', 'satellizer']);
 
-
+// production clientId: 1668067083447238
+// dev clientId: 449685485222786
 app.config(function ($routeProvider, $authProvider) {
 
   $authProvider.facebook({
-      clientId: '449685485222786',
+      clientId: '1668067083447238',
       scope: ['email'],
       scopeDelimiter: ',',
       profileFields: ['name', 'id', 'picture.type(large)', 'emails']
