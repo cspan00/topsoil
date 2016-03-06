b2552661921f3b92e1c5de7b5f103099ae51d083
Corey Yaspan <cspan00@gmail.com>
2016-03-06 16:12:50 -0700

    migration file update to include bool value

--- migrations/20160304151805_create_posts.js
+++ migrations/20160304151805_create_posts.js
@@ -3,11 +3,12 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('posts', function(t){
     t.increments();
     t.string('user_id');
-    t.string('facebook_id');
+    t.string('facebook_id').unique();
     t.string('title')
     t.string('address');
     t.string('lat');
     t.string('lng')
+    t.boolean('want')
     t.text('picture_url')
     t.text('description')
     t.timestamps();
