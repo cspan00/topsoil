f62c9494e46e18317676182450b684987a445a25
Corey Yaspan <cspan00@gmail.com>
2016-03-06 11:54:51 -0700

    include jquery in splashpage for scrolldown effect to info

---
+++ public/javascripts/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+  $(document).on('click', '.learnMore', function() {
+    var vheight = $(window).height();
+      $('html, body').animate({
+          scrollTop: (Math.floor($(window).scrollTop() / vheight)+1) * vheight
+        }, 1000);
+  })
+
+})
