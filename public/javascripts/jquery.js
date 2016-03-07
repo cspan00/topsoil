7a426c4869210ce2cacc523226e5c02d8baf31e3
Rachel Parri <erachelparri@gmail.com>
2016-03-06 20:14:15 -0700

    fixed scroll on splashpage

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -4,6 +4,6 @@
   $(document).on('click', '.learnMore', function() {
     var vheight = $(window).height();
       $('html, body').animate({
-          scrollTop: (Math.floor($(window).scrollTop() / vheight)+1) * vheight
+          scrollTop: (Math.floor($(window).scrollTop() / vheight)+2) * vheight
         }, 1000);
   })
