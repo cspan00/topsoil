19af4abb936e3cbb08fd3ae9ec858a423b21725a
Rachel Parri <erachelparri@gmail.com>
2016-03-06 20:31:28 -0700

    updated seed file

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -1,20 +1,9 @@
-
-
-
   $(document).on('click', '.learnMore', function() {
     var vheight = $(window).height();
       $('html, body').animate({
           scrollTop: (Math.floor($(window).scrollTop() / vheight)+2) * vheight
         }, 1000);
-  })
-
-  //
-  // $(document).on('click', '.backToTop', function() {
-  //   var vheight = $(window).height();
-  //     $('html, body').animate({
-  //         scrollTop: (Math.floor($(window).scrollTop() / vheight)+2) * vheight
-  //       }, 1000);
-  // })
+  });
 
   $(document).on('click', '.backToTop', function() {
     var vheight = $(window).height();
--- seeds/seed_posts.js
+++ seeds/seed_posts.js
@@ -6,6 +6,7 @@ exports.seed = function(knex, Promise) {
 
 
     knex('posts').insert({
+      want: 'true',
       author: 'Hermit the Crab',
       title: 'grow here please seriously',
       address: '123 fake st, des moines, IA',
@@ -13,7 +14,8 @@ exports.seed = function(knex, Promise) {
       description: 'The station was signed on in June 1994 by the original owner, Bay View Broadcasting. The original call letters were WXYQ and the station adopted an oldies format. Those call letters were previously used by an AM country station in Stevens Point, Wisconsin. Patrick Lopeman, who was also known as Pat Martin, was the principal owner and founder. WXYQ was sold to Crystal Clear Communications of Frankfort, Michigan for $200,000.00 in January, 1997. When new stations arrived in 1999, the new FCC rules allowed WXYQ to be consolidated into the Lake Michigan Broadcasting Group headed by Roger Baerwolf. When the FCC transfer was approved for $380,000.00 in 2000, Lake Michigan Broadcasting changed the call letters. The new call signs became WMTE-FM to reflect the co-ownership of its existing AM station WMTE. Baerwolf died suddenly in December 2004.'
     }),
     knex('posts').insert({
-      author: 'Mr. Crazy Bird', 
+      want: 'false',
+      author: 'Mr. Crazy Bird',
       title: 'I have a good place to grow watermelons and pears',
       address: 'your moms house',
       picture_url: 'http://www.funnyanimalsite.com/pictures/Crazy-bird.jpg',
