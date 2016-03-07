3bb7a15a0271e1fc3e359492db8ba3925e49faca
Rachel Parri <erachelparri@gmail.com>
2016-03-06 20:25:52 -0700

    cleaned up code

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -8,14 +8,6 @@
         }, 1000);
   })
 
-  //
-  // $(document).on('click', '.backToTop', function() {
-  //   var vheight = $(window).height();
-  //     $('html, body').animate({
-  //         scrollTop: (Math.floor($(window).scrollTop() / vheight)+2) * vheight
-  //       }, 1000);
-  // })
-
   $(document).on('click', '.backToTop', function() {
     var vheight = $(window).height();
     $("html, body").animate({
