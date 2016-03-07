cfc107a4b0e4f3a2e743a69533aab1d559287878
Rachel Parri <erachelparri@gmail.com>
2016-03-06 20:25:00 -0700

    added scroll to top option

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -7,3 +7,18 @@
           scrollTop: (Math.floor($(window).scrollTop() / vheight)+2) * vheight
         }, 1000);
   })
+
+  //
+  // $(document).on('click', '.backToTop', function() {
+  //   var vheight = $(window).height();
+  //     $('html, body').animate({
+  //         scrollTop: (Math.floor($(window).scrollTop() / vheight)+2) * vheight
+  //       }, 1000);
+  // })
+
+  $(document).on('click', '.backToTop', function() {
+    var vheight = $(window).height();
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000);
+  });
