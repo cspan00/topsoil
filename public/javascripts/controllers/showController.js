88567fdb6c2b8f048e66792527009f4b4553c6d7
Corey Yaspan <cspan00@gmail.com>
2016-03-08 17:50:11 -0700

    getting maps with a marker

--- public/javascripts/controllers/showController.js
+++ public/javascripts/controllers/showController.js
@@ -4,6 +4,22 @@ app.controller('showController', function($scope, $location, $routeParams, users
   users.getShowPost(id).then(function(results){
     console.log(results);
     $scope.showPost = results;
+    var mapOptions = {
+        zoom: 15,
+        center: new google.maps.LatLng(results.lat, results.lng),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var infoWindow = new google.maps.InfoWindow();
+
+
+      $scope.marker = new google.maps.Marker({
+          map: $scope.map,
+          position: new google.maps.LatLng(results.lat, results.lng),
+          title: results.title
+      })
+
   })
+
 
 })
